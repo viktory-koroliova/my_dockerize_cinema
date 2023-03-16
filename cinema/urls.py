0cdc5ec9127ad94b8a58f5f143b9f34f02bdace3
Vika <viktorykoroliova@gmail.com>
2023-03-16 18:01:11 +0200

    my_dockerize_cinema

---
+++ cinema/urls.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from rest_framework import routers
+
+from cinema.views import (
+    GenreViewSet,
+    ActorViewSet,
+    CinemaHallViewSet,
+    MovieViewSet,
+    MovieSessionViewSet,
+    OrderViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("cinema_halls", CinemaHallViewSet)
+router.register("movies", MovieViewSet)
+router.register("movie_sessions", MovieSessionViewSet)
+router.register("orders", OrderViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+app_name = "cinema"
